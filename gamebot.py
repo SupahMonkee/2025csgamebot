5e331bd8029366dae4a138343e771027ae6fea8a
Lucas Detlor <lucasjd0112@gmail.com>
2025-05-20 11:34:23 -0700

    v1

---
+++ gamebot.py
@@ -0,0 +1,18 @@
+import pyautogui as p
+import time as t
+
+while True:
+    t.sleep(0.5)
+    x = p.locateOnScreen(image ='test.png', confidence=0.2)
+    try:
+        #x = p.pixel(100,100)
+        x = p.locateOnScreen(image ='images/google.png', confidence=0.8)
+        p.moveTo(x)
+        x = p.locateOnScreen(image ='images/win.png', confidence=0.8)
+        p.moveTo(x)
+    except:
+        print('not found')
+
+
+#IT FINALLY WORKS
+# GET STARTED on BOT
