1403fd727ce8d5bfbf1fe9277741e81986f0cf3e
Lucas Detlor <lucasjd0112@gmail.com>
2025-05-30 11:34:55 -0700

    back from the wct

--- gamebot.py
+++ gamebot.py
@@ -1,18 +1,59 @@
 import pyautogui as p
 import time as t
 
-while True:
-    t.sleep(0.5)
-    x = p.locateOnScreen(image ='test.png', confidence=0.2)
+def start():
+    set = False
+    t.sleep(0.1)
     try:
-        #x = p.pixel(100,100)
-        x = p.locateOnScreen(image ='images/google.png', confidence=0.8)
-        p.moveTo(x)
-        x = p.locateOnScreen(image ='images/win.png', confidence=0.8)
-        p.moveTo(x)
+        try:
+            x = p.locateOnScreen(image='images/settings.png')
+            p.moveTo(x)
+            p.click()
+            cont = True
+        except:
+            x = p.locateOnScreen(image='images/settings2.png')
+            p.moveTo(x)
+            p.click()
+        set = True
+        if set is True:
+            try:
+                x = p.locateOnScreen(image='images/screeneffect.png')
+                p.moveTo(x)
+                p.click()
+                screeneff = True
+                if screeneff is True:
+                    x = p.locateOnScreen(image='images/back.png')
+                    p.moveTo(x)
+                    p.click()
+            except:
+                pass
     except:
-        print('not found')
+        set = True
+    if set is True:
+        try:
+            x = p.locateOnScreen(image='images/newgame.png')
+            p.moveTo(x)
+            p.click()
+        except:
+            pass
+    #if cont is True:  THIS IS FOR IF THERE IS A CONTINUE INSTEAD OF A NEW GAME, NEED TO CLICK YES TO RESTART, BEGIN HERE
+        #try:
+            #x = p.locateOnScreen()
+def tut():
+    tutorial = 0
+    t.sleep(0.1)
+    try:
+        x = p.locateOnScreen(image='images/ok.png')
+        p.moveTo(x)
+        p.click()
+        tutorial = 1
+    except:
+        pass
+    if tutorial == 1:
+        x = p.locateOnScreen(image='images/info.png')
+        p.moveTo(x)
+        p.click()
 
-
-#IT FINALLY WORKS
-# GET STARTED on BOT
+while True:
+    start()
+    tut()
